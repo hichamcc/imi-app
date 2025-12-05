a662448e74a20f3e9bdf21372b66c9e8f54663dd
hichamcc <gaddariowen@gmail.com>
2025-12-05 16:27:48 +0100

    add new countries to Driver

--- resources/views/drivers/create.blade.php
+++ resources/views/drivers/create.blade.php
@@ -107,13 +107,14 @@
                                 <option value="">{{ __('Select Country') }}</option>
                                 @php
                                     $countries = [
-                                        'AT' => 'Austria', 'BE' => 'Belgium', 'BG' => 'Bulgaria', 'HR' => 'Croatia',
+                                        'AT' => 'Austria', 'BY' => 'Belarus', 'BE' => 'Belgium', 'BG' => 'Bulgaria', 'HR' => 'Croatia',
                                         'CY' => 'Cyprus', 'CZ' => 'Czech Republic', 'DK' => 'Denmark', 'EE' => 'Estonia',
                                         'FI' => 'Finland', 'FR' => 'France', 'DE' => 'Germany', 'GR' => 'Greece',
                                         'HU' => 'Hungary', 'IE' => 'Ireland', 'IT' => 'Italy', 'LV' => 'Latvia',
                                         'LT' => 'Lithuania', 'LU' => 'Luxembourg', 'MT' => 'Malta', 'NL' => 'Netherlands',
                                         'PH' => 'Philippines', 'PL' => 'Poland', 'PT' => 'Portugal', 'RO' => 'Romania', 'SK' => 'Slovakia',
-                                        'SI' => 'Slovenia', 'ES' => 'Spain', 'SE' => 'Sweden'
+                                        'SI' => 'Slovenia', 'ES' => 'Spain', 'LK' => 'Sri Lanka', 'SE' => 'Sweden', 'UA' => 'Ukraine',
+                                        'ZW' => 'Zimbabwe'
                                     ];
                                 @endphp
                                 @foreach($countries as $code => $name)
