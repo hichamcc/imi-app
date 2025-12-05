0632e4bd3048aeea2015ddb281a6f204fcf654f0
hichamcc <gaddariowen@gmail.com>
2025-12-05 15:30:40 +0100

    add Issuing Country PH

--- resources/views/drivers/edit.blade.php
+++ resources/views/drivers/edit.blade.php
@@ -123,7 +123,7 @@
                                         'FI' => 'Finland', 'FR' => 'France', 'DE' => 'Germany', 'GR' => 'Greece',
                                         'HU' => 'Hungary', 'IE' => 'Ireland', 'IT' => 'Italy', 'LV' => 'Latvia',
                                         'LT' => 'Lithuania', 'LU' => 'Luxembourg', 'MT' => 'Malta', 'NL' => 'Netherlands',
-                                        'PL' => 'Poland', 'PT' => 'Portugal', 'RO' => 'Romania', 'SK' => 'Slovakia',
+                                        'PH' => 'Philippines', 'PL' => 'Poland', 'PT' => 'Portugal', 'RO' => 'Romania', 'SK' => 'Slovakia',
                                         'SI' => 'Slovenia', 'ES' => 'Spain', 'SE' => 'Sweden'
                                     ];
                                 @endphp
